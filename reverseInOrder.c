992ab464495b7895eb95678d6b8335a09dfc6d22
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-01-07 09:36:29 +0600

    first commit

---
+++ reverseInOrder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Scanning in elements: \n");
+
+    for (int i=0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Printing in reverse: \n");
+    for (int i=n-1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
